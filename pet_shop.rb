93353873f66d9ce5108bc942e8b581018011872e
Tom Benham <benji26@hotmail.co.uk>
2016-09-30 14:56:23 +0100

    4 tests done

--- pet_shop.rb
+++ pet_shop.rb
@@ -4,5 +4,11 @@ def pet_shop_name(shop)
   return shop [:name]
 end
 
-def 
+def total_cash(total)
+  return total[:admin][:total_cash]
 end
+
+def add_or_remove_cash(shop, transaction)
+  return shop[:admin][:total_cash] += transaction
+end
+
