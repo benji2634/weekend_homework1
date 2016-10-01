a618ef0609d4686221339ca28293287cff14c1f0
Tom Benham <benji26@hotmail.co.uk>
2016-10-01 11:29:50 +0100

    7th test done

--- pet_shop.rb
+++ pet_shop.rb
@@ -12,3 +12,15 @@ def add_or_remove_cash(shop, transaction)
   return shop[:admin][:total_cash] += transaction
 end
 
+def pets_sold(sold)
+  return sold[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pets, add_pets)
+  return pets[:admin][:pets_sold] += add_pets
+end
+
+def stock_count(stock)
+  return stock[:pets].length
+end
+
