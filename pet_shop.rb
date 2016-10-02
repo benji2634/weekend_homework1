24ad2e1f626fdc22d1983374f39d906933c7b195
Tom Benham <benji26@hotmail.co.uk>
2016-10-02 22:58:05 +0100

    adapted sell_pet_to_customer to make test 19 work

--- pet_shop.rb
+++ pet_shop.rb
@@ -72,7 +72,7 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 def sell_pet_to_customer(shop, pet, customer)
-  if customer_can_afford_pet(customer, pet)
+  if !pet.nil? 
     add_pet_to_customer(customer, pet)
     increase_pets_sold(shop, 1)
     add_or_remove_cash(shop, pet[:price])
